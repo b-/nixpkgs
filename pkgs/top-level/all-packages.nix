ff4dbf5e5e6ad68b8000d5e77cb2b121e0ac9b01
Nikolay Korotkiy <sikmir@disroot.org>
2024-02-17 23:45:06 +0400

    osm2pgsql: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33837,8 +33837,6 @@ with pkgs;
 
   opsdroid = callPackage ../applications/networking/opsdroid { };
 
-  osm2pgsql = callPackage ../tools/misc/osm2pgsql { };
-
   ostinato = libsForQt5.callPackage ../applications/networking/ostinato {
     protobuf = protobuf_21;
   };
