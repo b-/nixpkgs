7a1bb26b17cadfa1b43a0807d6dda9c2872a6bb1
Thomas Heijligen <src@posteo.de>
2024-02-19 21:29:43 +0000

    gnatPackages: Build all ada-modules with gnat12 and gnat13
    Add namespace gnat12Packages and gnat13Packages to get ada-modules build
    with a specific gnat version.
    For gnatprove this means building the gnat12 and gnat13 version.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15997,7 +15997,7 @@ with pkgs;
     inherit (gnome2) libart_lgpl;
   });
 
-  gnat = gnat12;
+  gnat = gnat12; # When changing this, update also gnatPackages
 
   gnat11 = wrapCC (gcc11.cc.override {
     name = "gnat";
@@ -16069,7 +16069,10 @@ with pkgs;
     bintools = bintoolsDualAs;
   });
 
-  gnatPackages   = recurseIntoAttrs (callPackage ./ada-packages.nix { gnat = buildPackages.gnat; });
+  gnat12Packages = recurseIntoAttrs (callPackage ./ada-packages.nix { gnat = buildPackages.gnat12; });
+  gnat13Packages = recurseIntoAttrs (callPackage ./ada-packages.nix { gnat = buildPackages.gnat13; });
+  gnatPackages   = gnat12Packages;
+
   inherit (gnatPackages)
     gprbuild
     gnatprove;
