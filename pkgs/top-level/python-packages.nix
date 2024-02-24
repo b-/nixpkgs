1ae3bf4a78e9da73c2fa2d49f4331100dce3bafa
Robert Schütz <nix@dotlambda.de>
2024-02-24 07:42:01 -0800

    python311Packages.systembridgemodels: init at 4.0.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14224,6 +14224,8 @@ self: super: with self; {
 
   systembridge = callPackage ../development/python-modules/systembridge { };
 
+  systembridgemodels = callPackage ../development/python-modules/systembridgemodels { };
+
   systemd = callPackage ../development/python-modules/systemd {
     inherit (pkgs) systemd;
   };
